95580f2a89f45382defdc763bad496a3a01e76c7
Paweł Tarnowski <pawel.tarnowski.it@gmail.com>
2021-04-15 19:35:04 +0200

    Moved to sql

---
+++ API/Data/Migrations/20210414213214_UserPasswordCorrection.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class UserPasswordCorrection : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PasswodHash",
+                table: "Users",
+                newName: "PasswordHash");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PasswordHash",
+                table: "Users",
+                newName: "PasswodHash");
+        }
+    }
+}
